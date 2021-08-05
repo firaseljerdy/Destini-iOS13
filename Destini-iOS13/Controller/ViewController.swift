07efe77cee8d5dd73f7a193765a18a48867b4671
Firas El-Jerdy <firas.aljerdy@gmail.com>
2021-08-05 20:15:15 +0300

    MVC Ready

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,11 +13,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    var destinations: [Int] = []
+    var currentStoryIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(currentStoryIndex)
+        currentStoryIndex = storyBrain.getCurrentStoryIndex()
+        fetchStoryData(story: currentStoryIndex)
     }
+    
+    func fetchStoryData(story: Int) {
+        
+        storyLabel.text = storyBrain.getStoryText(index: story)
+        let choices = [storyBrain.getStoryChoices(index: story).0, storyBrain.getStoryChoices(index: story).1]
+        
+        destinations = [storyBrain.getCurrentStoryDestinations(index: story).0, storyBrain.getCurrentStoryDestinations(index: story).1]
+        
+        choice1Button.setTitle(choices[0], for: .normal)
+        choice2Button.setTitle(choices[1], for: .normal)
+    }
+    
+    @IBAction func onChoiceButtonPress(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        
+        storyBrain.progressStory(storyIndex: currentStoryIndex, userAnswer: userAnswer!)
+        
+        currentStoryIndex = storyBrain.getCurrentStoryIndex()
 
-
+        fetchStoryData(story: currentStoryIndex)
+        
+        print(currentStoryIndex, storyBrain.getCurrentStoryIndex())
+    }
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,9 +1,18 @@
-//
-//  Sotry.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import Foundation
+
+struct Story {
+    var storyText: String
+    var choice1: String
+    var choice1Destination: Int
+    var choice2: String
+    var choice2Destination: Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.storyText = title
+        self.choice1 = choice1
+        self.choice1Destination = choice1Destination
+        self.choice2 = choice2
+        self.choice2Destination = choice2Destination
+    }
+}
